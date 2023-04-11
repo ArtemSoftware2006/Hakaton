b6a73025e91fec4e3fc97c3f119aa3a3dfd6d188
Artem <artem.egorov.art2006@gmail.com>
2023-04-11 10:59:00 +0300

    Реализовал IProposalRepository, Proposalrepository

---
+++ DAL/Repository/ProposalRepository.cs
@@ -0,0 +1,53 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class ProposalRepository : IProposalRepository
+    {
+        public ProposalRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public AppDbContext _dbContext { get; set; }
+        public async Task<bool> Create(Proposal entity)
+        {
+            await _dbContext.Proposals.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Delete(Proposal entity)
+        {
+            _dbContext.Proposals.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Proposal> Get(int id)
+        {
+            return await _dbContext.Proposals.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public IQueryable<Proposal> GetAll()
+        {
+            return _dbContext.Proposals;
+        }
+
+        public async Task<Proposal> Update(Proposal entity)
+        {
+            _dbContext.Proposals.Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
