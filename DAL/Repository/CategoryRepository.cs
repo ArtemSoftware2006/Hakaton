93f3720c9502dc9da5bc5cb174db4f7cc4ffa99a
Artem <artem.egorov.art2006@gmail.com>
2023-04-11 13:43:22 +0300

    Добавил ICategoryRepository, CategoryRepository

---
+++ DAL/Repository/CategoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using DAL.Interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public AppDbContext _dbContext { get; set; }
+
+        public CategoryRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Create(Category entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Delete(Category entity)
+        {
+            _dbContext.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Category> Get(int id)
+        {
+            return await _dbContext.Categories.FirstOrDefaultAsync(x=>x.Id == id);
+        }
+
+        public IQueryable<Category> GetAll()
+        {
+            return _dbContext.Categories;
+        }
+
+        public async Task<Category> Update(Category entity)
+        {
+            _dbContext.Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+    }
+}
