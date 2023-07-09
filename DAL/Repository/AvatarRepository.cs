48c03aef48c9f40480e1397f31b3a2dc2c2c7ee7
Artem <artem.egorov.art2006@gmail.com>
2023-07-09 20:18:49 +0300

    Реализовал IAvatarRepository. Убрал using

---
+++ DAL/Repository/AvatarRepository.cs
@@ -0,0 +1,47 @@
+using DAL.Interfaces;
+using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repository
+{
+    public class AvatarRepository : IAvatarRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public AvatarRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> Create(Avatar entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> Delete(Avatar entity)
+        {
+             _dbContext.Remove(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Avatar> Get(int id)
+        {
+            return await _dbContext.Avatars.FirstOrDefaultAsync(x=>x.Id == id);
+        }
+
+        public IQueryable<Avatar> GetAll()
+        {
+            return _dbContext.Avatars;
+        }
+
+        public async Task<Avatar> Update(Avatar entity)
+        {
+            _dbContext.Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;        }
+    }
+}
